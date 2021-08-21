efd1239c29c1126426e9cf30d8c480e46402bf7d
kalyanraju <82213052+kalyan-maker@users.noreply.github.com>
2021-08-21 13:53:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+  //Run code based on a given condition
+
+  const [{ token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      spotify.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.getPlaylist("2tx5dsv8mAu08pKtycGoLY").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      spotify.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotify,
+      });
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+    }
+  }, [token, dispatch]);
+
+  return (
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={spotify} />}
+    </div>
+  );
+}
+
+export default App;
